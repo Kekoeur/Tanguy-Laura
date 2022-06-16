0e315ac63dbb73780e49bfa7a1c4862073ddb110
Christo26.g <Christo26.g@gmail.com>
2022-06-16 20:46:10 +0200

    /gallery

---
+++ js/gallery.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4) {
+            console.log(xhr.response);
+            document.getElementById('img').innerHTML =  xhr.response;
+        }
+    }
+    xhr.open('POST', '../php/display_files.php', true);
+    xhr.send('');
+});
